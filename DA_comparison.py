05808cc225aea66d6497ecfe335f0d22b2536799
Maximilian Balandat <balandat@eecs.berkeley.edu>
2015-05-06 13:12:28 -0700

    pass file name and time stamp to _results functions

--- DA_comparison.py
+++ DA_comparison.py
@@ -8,18 +8,19 @@ Comparison of Dual Averaging algorithms
 # Set up infrastructure and basic problem parameters
 import multiprocessing as mp
 import numpy as np
+from datetime import datetime
 from ContNoRegret.Domains import nBox, UnionOfDisjointnBoxes
-from ContNoRegret.LossFunctions import AffineLossFunction
+from ContNoRegret.LossFunctions import AffineLossFunction, QuadraticLossFunction, random_AffineLosses, random_QuadraticLosses
 from ContNoRegret.NoRegretAlgos import ContNoRegretProblem
-from ContNoRegret.utils import random_AffineLosses, random_QuadraticLosses, plot_results, save_results, CNR_worker
-from ContNoRegret.DualAveraging import ExponentialPotential, IdentityPotential, CompositeOmegaPotential, pNormPotential, LogtasticPotential
+from ContNoRegret.utils import CNR_worker, plot_results, save_results
+from ContNoRegret.DualAveraging import ExponentialPotential, IdentityPotential, CompositeOmegaPotential, pNormPotential, LogtasticPotential, HuberPotential
 
 # this is the location of the folder for the results
 results_path = '/Users/balandat/Documents/Code/Continuous_No-Regret/results/'
 desc = 'DA_Comparison'
-tmpfolder = '/Volumes/tmp/' # if possible choose this to be a RamDisk
+tmpfolder = '/Volumes/tmp/' # if possible, choose this to be a RamDisk
 
-T = 1000 # Time horizon
+T = 2500 # Time horizon
 M = 10.0 # Uniform bound on the funciton (L-infinity norm)
 Lbnd = 5.0 # Uniform bound on the Lipschitz constant
 N = 2500 # Number of parallel algorithm instances
@@ -29,12 +30,13 @@ dom = nBox([(-1,1), (-1,1)])#, (-1,1)]) # domain is a unit ball (1-norm) in 2D
 
 # Now create some random loss functions
 # d=2 means sampling the a vector uniformly at random from {x : ||x||_2<L}}
-lossfuncs, M = random_AffineLosses(dom, Lbnd, T, d=2)
-# 
-# epsilon = 0.3
-# mus = ((1-epsilon)*(0.5 + 0.5*np.array([np.sin(np.linspace(0,2*np.pi,T)), np.cos(np.linspace(0,2*np.pi,T))])).T 
-#        + epsilon*dom.sample_uniform(T))
-# lossfuncs = random_QuadraticLosses(dom, mus, Lbnd, M)
+# lossfuncs, M = random_AffineLosses(dom, Lbnd, T, d=2)
+
+mus_random = dom.sample_uniform(T)
+epsilon = 0.3
+mus = ((1-epsilon)*(0.5 + 0.5*np.array([np.sin(np.linspace(0,2*np.pi,T)), np.cos(np.linspace(0,2*np.pi,T))])).T 
+       + epsilon*mus_random)
+lossfuncs = random_QuadraticLosses(dom, mus_random, Lbnd, M)
 
 # normbounds = {'1': [lossfunc.norm(1) for lossfunc in lossfuncs],
 #               '2': [lossfunc.norm(2) for lossfunc in lossfuncs]}
@@ -51,15 +53,16 @@ potentials = [ExponentialPotential(), CompositeOmegaPotential(gamma=2), Logtasti
 # potentials = [pNormPotential(1.25), pNormPotential(1.5), pNormPotential(1.75), pNormPotential(2.0)]
 
 # the following runs fine if the script is the __main__ method, but crashes when running from ipython
-pool = mp.Pool(processes=4)
+pool = mp.Pool(processes=7)
 
 kwargs = [{'alphas':alphas, 'thetas':thetas, 'Ngrid':Ngrid, 'potential':pot, 
            'pid':i, 'tmpfolder':tmpfolder, 'label':pot.desc} for i,pot in enumerate(potentials)]
 processes = [pool.apply_async(CNR_worker, (prob, N, 'DA'), kwarg) for kwarg in kwargs]
 results = [process.get() for process in processes]
 
-plot_results(results, offset=100, path=results_path)
-save_results(results, path=results_path)  
+timenow = datetime.now().strftime('%Y-%m-%d_%H-%M') 
+plot_results(results, offset=100, path=results_path, timestamp=timenow)
+save_results(results, __file__, path=results_path, timestamp=timenow)  
 
 
 
