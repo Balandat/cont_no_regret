b89002bcc62f75d9a16643b69a344a6f83f858bb
Maximilian Balandat <balandat@eecs.berkeley.edu>
2015-05-08 16:39:27 -0700

    Commented out some deprecated Lossfunctions

--- ContNoRegret/LossFunctions.py
+++ ContNoRegret/LossFunctions.py
@@ -16,7 +16,7 @@ from mpl_toolkits.mplot3d import axes3d
 from scipy.linalg import orth, eigh
 from scipy.integrate import nquad
 from scipy.stats import uniform, gamma
-from ContNoRegret.Distributions import Gaussian
+# from ContNoRegret.Distributions import Gaussian
 from ContNoRegret.Domains import nBox, UnionOfDisjointnBoxes, DifferenceOfnBoxes
 
 
@@ -65,7 +65,6 @@ class LossFunction(object):
         grad_step = x - step*self.grad(x)
         return self.domain.project(grad_step)
     
-
    
 
 class AffineLossFunction(LossFunction):
@@ -103,8 +102,9 @@ class AffineLossFunction(LossFunction):
                              + 'are supported for computing minimum and maximum of AffineLossFunctions'))
 
     def grad(self, points): 
+        """ Returns the gradient of the AffineLossFunction (equal at all points) """
         return np.repeat(np.array(self.a, ndmin=2), points.shape[0], axis=0)
-    
+        
     def __add__(self, affine2):
         """ Add two AffineLossFunction objects (assumes that both functions
             are defined over the same domain. """
@@ -302,6 +302,17 @@ class PolynomialLossFunction(LossFunction):
     def val(self, points):
         monoms = np.array([points**exps for exps in self.polydict.keys()]).prod(2)
         return np.sum([monom*coeff for monom,coeff in zip(monoms, self.polydict.values())], axis=0)
+    
+    def max(self):
+        """ Compute the maximum of the loss function over the domain. """
+        return NotImplementedError
+
+    def min(self):
+        """ Compute the minimum of the loss function over the domain. """
+        return NotImplementedError
+    
+    def grad(self, points): 
+        return NotImplementedError
     
     def __add__(self, poly2):
         """ Add two PolynomialLossFunction objects (assumes that both polynomials 
@@ -519,69 +530,69 @@ def isPosDef(Q):
 # The functions below are deprecated and to be removed at a later stage
 #######################################################################
 
-class GaussianLossFunction(LossFunction):
-    """ Loss given by l(s) = M(1 - gamma*exp[-0.5*(s-x)^T Sigma^-1 (s-x)]) """ 
-    
-    def __init__(self, domain, mean, cov, M):
-        self.domain, self.M = domain, M
-        self.p = Gaussian(domain, mean, cov)
-        self.L = self.computeL()
-        self.Cnorm = 1/self.p.density(mean)
-    
-    def val(self, points):
-        return self.M*(1 - self.Cnorm*self.p.density(points))
-    
-    def min(self):
-        """ For now just returns 1-peak, need to do something smarter... """
-        return self.M*(1 - self.Cnorm*self.p.density(self.p.mean))
-    
-    def grad(self, points): 
-        return - self.M*self.Cnorm*self.p.grad_density(points)
-    
-    def Hessian(self, points): 
-        return - self.M*self.Cnorm*self.p.Hessian_density(points)
-    
-    def computeL(self):
-        """ Computes a Lipschitz constant for the loss function. """
-        lambdamin = eigh(self.p.cov, eigvals=(0,0), eigvals_only=True)
-        return self.M*((2*np.pi)**self.domain.n*np.e*np.linalg.det(self.p.cov)*lambdamin)**(-0.5)
-
-
-class CumulativeLoss(LossFunction):
-    """ Class for cumulative loss function objects """
-    
-    def __init__(self, lossfuncs):
-        """ Constructor. Here lossfuncs is a list of LossFunction objects """
-        # check that domains are the same, then call superclass constructor        
-        super(LossFunction, self).__init__(lossfuncs[0].domain)
-        self.lossfuncs = lossfuncs
-        
-    def sample(self, N):
-        """ Draw N independent samples from the distribution """
-        indices = np.random.choice(len(self.weights), N, p=self.weights/np.sum(self.weights))
-        return np.concatenate([self.distributions[index].sample(1) for index in indices])
-    
-    def add(self, lossfunc):
-        """ Adds the loss function to the cumulative loss """
-        self.lossfuncs.append(lossfunc)
-        
-    def val(self, points):
-        """ Returns the cumulative loss for each of the points in points """
-        return np.array(np.sum(np.array([lossfunc.val(points) for lossfunc in self.lossfuncs]), axis=0), ndmin=1)
-    
-    def grad(self, points): 
-        """ Returns gradient of the cumulative loss function at the specified points """
-        # the following looks weird but it allows to vectorize everything.
-        return np.sum(np.array([lossfunc.grad(points) for lossfunc in self.lossfuncs]), axis=0)
-    
-    def Hessian(self, points): 
-        """ Returns Hessian of the cumulative loss function at the specified points """       
-        return np.sum(np.array([lossfunc.Hessian(points) for lossfunc in self.lossfuncs]), axis=0)
-  
-    def upperbound_val(self):
-        """ Computes the a very crude upper bound of the oveall maximum based on the 
-            maxima of the individual loss functions """
-        return np.sum(np.array([lossfunc.max() for lossfunc in self.lossfuncs]))
+# class GaussianLossFunction(LossFunction):
+#     """ Loss given by l(s) = M(1 - gamma*exp[-0.5*(s-x)^T Sigma^-1 (s-x)]) """ 
+#     
+#     def __init__(self, domain, mean, cov, M):
+#         self.domain, self.M = domain, M
+#         self.p = Gaussian(domain, mean, cov)
+#         self.L = self.computeL()
+#         self.Cnorm = 1/self.p.density(mean)
+#     
+#     def val(self, points):
+#         return self.M*(1 - self.Cnorm*self.p.density(points))
+#     
+#     def min(self):
+#         """ For now just returns 1-peak, need to do something smarter... """
+#         return self.M*(1 - self.Cnorm*self.p.density(self.p.mean))
+#     
+#     def grad(self, points): 
+#         return - self.M*self.Cnorm*self.p.grad_density(points)
+#     
+#     def Hessian(self, points): 
+#         return - self.M*self.Cnorm*self.p.Hessian_density(points)
+#     
+#     def computeL(self):
+#         """ Computes a Lipschitz constant for the loss function. """
+#         lambdamin = eigh(self.p.cov, eigvals=(0,0), eigvals_only=True)
+#         return self.M*((2*np.pi)**self.domain.n*np.e*np.linalg.det(self.p.cov)*lambdamin)**(-0.5)
+# 
+# 
+# class CumulativeLoss(LossFunction):
+#     """ Class for cumulative loss function objects """
+#     
+#     def __init__(self, lossfuncs):
+#         """ Constructor. Here lossfuncs is a list of LossFunction objects """
+#         # check that domains are the same, then call superclass constructor        
+#         super(LossFunction, self).__init__(lossfuncs[0].domain)
+#         self.lossfuncs = lossfuncs
+#         
+#     def sample(self, N):
+#         """ Draw N independent samples from the distribution """
+#         indices = np.random.choice(len(self.weights), N, p=self.weights/np.sum(self.weights))
+#         return np.concatenate([self.distributions[index].sample(1) for index in indices])
+#     
+#     def add(self, lossfunc):
+#         """ Adds the loss function to the cumulative loss """
+#         self.lossfuncs.append(lossfunc)
+#         
+#     def val(self, points):
+#         """ Returns the cumulative loss for each of the points in points """
+#         return np.array(np.sum(np.array([lossfunc.val(points) for lossfunc in self.lossfuncs]), axis=0), ndmin=1)
+#     
+#     def grad(self, points): 
+#         """ Returns gradient of the cumulative loss function at the specified points """
+#         # the following looks weird but it allows to vectorize everything.
+#         return np.sum(np.array([lossfunc.grad(points) for lossfunc in self.lossfuncs]), axis=0)
+#     
+#     def Hessian(self, points): 
+#         """ Returns Hessian of the cumulative loss function at the specified points """       
+#         return np.sum(np.array([lossfunc.Hessian(points) for lossfunc in self.lossfuncs]), axis=0)
+#   
+#     def upperbound_val(self):
+#         """ Computes the a very crude upper bound of the oveall maximum based on the 
+#             maxima of the individual loss functions """
+#         return np.sum(np.array([lossfunc.max() for lossfunc in self.lossfuncs]))
     
     
     
