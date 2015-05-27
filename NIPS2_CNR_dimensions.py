09719eb702bbc85eeb1bbba63185bb8078e16d9d
Balandat <balandat@gmail.com>
2015-05-27 14:00:29 -0700

    server-side update of dimension. Another try....

--- NIPS2_CNR_dimensions.py
+++ NIPS2_CNR_dimensions.py
@@ -17,13 +17,12 @@ from ContNoRegret.LossFunctions import QuadraticLossFunction, random_QuadraticLo
 from ContNoRegret.NoRegretAlgos import ContNoRegretProblem
 from ContNoRegret.utils import CNR_worker, plot_results, save_results, circular_tour
 from ContNoRegret.animate import save_animations
-from ContNoRegret.Potentials import (ExponentialPotential, IdentityPotential, pNormPotential, CompositePotential,
-                                        ExpPPotential, pExpPotential, HuberPotential, LogtasticPotential, FractionalLinearPotential)
+from ContNoRegret.Potentials import ExponentialPotential, pNormPotential
 
 # this is the location of the folder for the results
 results_path = '/home/max/Documents/CNR_results/'
 desc = 'NIPS2_CNR_dimensions'
-tmpfolder = '/Volumes/tmp/' # if possible, choose this to be a RamDisk
+tmpfolder = '/media/tmp/' # if possible, choose this to be a RamDisk
 
 # some flags for keeping a record of the simulation parameters
 save_res = True
@@ -31,7 +30,7 @@ show_plots = False
 save_anims = False
 show_anims = False
 
-T = 5000 # Time horizon
+T = 50 # Time horizon
 M = 10.0 # Uniform bound on the function (in the dual norm)
 L = 5.0 # Uniform bound on the Lipschitz constant
 N = 2500 # Number of parallel algorithm instances
@@ -49,44 +48,43 @@ alpha_ec = []
 
 doms = []
 # recover the losses from previous run for comparison. 
-for i,n in enumerate([3,4]):
-    with open('/Users/balandat/Documents/Code/Continuous_No-Regret/individ_results/dimension/ExpPot_n{}.piggl'.format(n), 'rb') as f:
-        result = pickle.load(f)                
-    problems.append(result.problem)
-    doms.append(problems[-1].domain)
-    Dmus = [doms[-1].compute_Dmu(loss.mu) for loss in problems[-1].lossfuncs]
-    lambdamax = np.max([np.min((L/Dmu, 2*M/Dmu**2)) for Dmu in Dmus])
-    alpha_ec.append(H/lambdamax**2) 
+#for i,n in enumerate([2,3,4]):
+#    with open('/Users/balandat/Documents/Code/Continuous_No-Regret/individ_results/dimension/ExpPot_n{}.piggl'.format(n), 'rb') as f:
+#        result = pickle.load(f)                
+#    problems.append(result.problem)
+#    doms.append(problems[-1].domain)
+#    Dmus = [doms[-1].compute_Dmu(loss.mu) for loss in problems[-1].lossfuncs]
+#    lambdamax = np.max([np.min((L/Dmu, 2*M/Dmu**2)) for Dmu in Dmus])
+#    alpha_ec.append(H/lambdamax**2) 
 
 
 # # loop over the domains with different dimension
-#for dom in doms:
-#
-#    # Now create some random loss functions
-#    mus = dom.sample_uniform(T)
-#    lossfuncs, Mnew, lambdamax = random_QuadraticLosses(dom, mus, L, M, pd=True, H=H)
-#    alpha_ec.append(H/lambdamax**2)
-#    # M2 = np.max([lossfunc.norm(2, tmpfolder=tmpfolder) for lossfunc in lossfuncs])
-#
-#    # create the problem
-#    problems.append(ContNoRegretProblem(dom, lossfuncs, L, Mnew, desc=desc))
+for dom in doms:
+
+    # Now create some random loss functions
+    mus = dom.sample_uniform(T)
+    lossfuncs, Mnew, lambdamax = random_QuadraticLosses(dom, mus, L, M, pd=True, H=H)
+    alpha_ec.append(H/lambdamax**2)
+    M4 = np.max([lossfunc.norm(2, tmpfolder=tmpfolder) for lossfunc in lossfuncs])
+    # create the problem
+    problems.append(ContNoRegretProblem(dom, lossfuncs, L, Mnew, desc=desc))
   
 # Select a couple of potentials for the Dual Averaging algorithm
-potentials = [pNormPotential(1.5)]
+potentials = [ExponentialPotential(), pNormPotential(1.5, M=M4)]
   
 # the following runs fine if the script is the __main__ method, but crashes when running from ipython
 #pool = mp.Pool(processes=mp.cpu_count()-1
-pool = mp.Pool(4)
+pool = mp.Pool(4))
 processes = []
 
 for i,prob in enumerate(problems):
     for pot in potentials:
         processes.append(pool.apply_async(CNR_worker, (prob,N,'DA'), {'opt_rate':True, 'Ngrid':Ngrid, 
 					  'potential':pot, 'pid':len(processes), 'tmpfolder':tmpfolder, 'label':pot.desc})) 
-    #processes.append(pool.apply_async(CNR_worker, (prob,N,'GP'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'GP', 'tmpfolder':tmpfolder}))
-    #processes.append(pool.apply_async(CNR_worker, (prob,N,'OGD'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'OGD', 'tmpfolder':tmpfolder, 'H':H}))
-    #processes.append(pool.apply_async(CNR_worker, (prob,N,'ONS'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'ONS', 'tmpfolder':tmpfolder, 'alpha':alpha_ec[i]}))
-    #processes.append(pool.apply_async(CNR_worker, (prob,N,'FTAL'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'FTAL', 'tmpfolder':tmpfolder, 'alpha':alpha_ec[i]}))
+    processes.append(pool.apply_async(CNR_worker, (prob,N,'GP'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'GP', 'tmpfolder':tmpfolder}))
+    processes.append(pool.apply_async(CNR_worker, (prob,N,'OGD'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'OGD', 'tmpfolder':tmpfolder, 'H':H}))
+    processes.append(pool.apply_async(CNR_worker, (prob,N,'ONS'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'ONS', 'tmpfolder':tmpfolder, 'alpha':alpha_ec[i]}))
+    processes.append(pool.apply_async(CNR_worker, (prob,N,'FTAL'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'FTAL', 'tmpfolder':tmpfolder, 'alpha':alpha_ec[i]}))
     if prob.domain.n == 4:
         processes.append(pool.apply_async(CNR_worker, (prob,N,'EWOO'), {'Ngrid':Ngrid, 'pid':len(processes), 'label':'EWOO', 'tmpfolder':tmpfolder, 'alpha':alpha_ec[i]}))
         
@@ -103,8 +101,7 @@ for i,prob in enumerate(problems):
 #processes += [pool.apply_async(CNR_worker, (prob, N, 'EWOO'), EWOOkwargs) for prob in problems]
 
 # wait for the processes to finish an collect the results (as file handlers)
-resultfiles = [process.get() for process in processes]
-print(resultfiles)
+results = [process.get() for process in processes]
 
 # read the results from file
 #results = []
@@ -113,18 +110,18 @@ print(resultfiles)
 #        results.append(pickle.load(f))
 #
 # plot results and/or save a persistent copy (pickled) of the detailed results
-#timenow = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')# create a time stamp for unambiguously naming the results folder
-#results_directory = '{}{}/'.format(results_path, timenow)
-#  
-#if save_res:   
-#    os.makedirs(results_directory, exist_ok=True) # this could probably use a safer implementation
-#    plot_results(results, 100, results_directory, show_plots)
-#    if save_anims:
-#        save_animations(results, 10, results_directory, show_anims)  
-#    save_results(results, results_directory)  
-    # store the previously read-in contents of this file in the results folder
-#    with open(results_directory+str(__file__), 'w') as f:
-#        f.write(thisfile)
-#else:
-#    plot_results(results, offset=100)
+timenow = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M')# create a time stamp for unambiguously naming the results folder
+results_directory = '{}{}/'.format(results_path, timenow)
+  
+if save_res:   
+    os.makedirs(results_directory, exist_ok=True) # this could probably use a safer implementation
+    plot_results(results, 100, results_directory, show_plots)
+    if save_anims:
+        save_animations(results, 10, results_directory, show_anims)  
+    save_results(results, results_directory)  
+   # store the previously read-in contents of this file in the results folder
+    with open(results_directory+str(__file__), 'w') as f:
+        f.write(thisfile)
+else:
+    plot_results(results, offset=100)
 
